e6e7c2cfcec6c22fbe5c4d8727f3f39c97994df7
Valentin Trujillo <alexandro4v@gmail.com>
2024-04-27 00:13:22 +0200

    Main app structure changed

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_pokedex/home_screen.dart';
+import 'package:network/di.dart' as network;
+import 'package:pokedex/di.dart' as pokedex;
+import 'package:pokemon/di.dart' as pokemon;
+import 'package:storage/di.dart' as storage;
 
-void main() {
+import 'di.dart';
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await network.init();
+  await storage.init();
+  await pokemon.init();
+  await pokedex.init();
+  await init();
   runApp(const Pokedex());
 }
 
@@ -15,22 +29,10 @@ class Pokedex extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Home(),
-    );
-  }
-}
-
-class Home extends StatelessWidget {
-  const Home({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('Pokédex Code Challenge'),
+      home: MultiBlocProvider(
+        providers: const [],
+        child: const HomeScreen(),
       ),
-      body: const Center(child: Text('Lee el README para comenzar')),
     );
   }
 }
