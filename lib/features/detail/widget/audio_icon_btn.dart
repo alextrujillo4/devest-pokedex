b8e67b07fa506c0e961b72c6e143c7de21c9332d
Valentin Trujillo <alexandro4v@gmail.com>
2024-04-27 17:12:33 +0200

    audio_icon_btn.dart created

---
+++ lib/features/detail/widget/audio_icon_btn.dart
@@ -0,0 +1,30 @@
+import 'package:audioplayers/audioplayers.dart';
+import 'package:flowery_tts/flowery_tts.dart';
+import 'package:flutter/material.dart';
+
+class AudioIconButton extends StatelessWidget {
+  final Flowery flowery;
+  final AudioPlayer player;
+  final String description;
+
+  const AudioIconButton({
+    Key? key,
+    required this.flowery,
+    required this.player,
+    required this.description,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return IconButton(
+      onPressed: () async {
+        final soundBytes = await flowery.tts(text: description, voice: 'Guy', speed: 1.2);
+        await player.play(BytesSource(soundBytes));
+      },
+      icon: const Icon(
+        Icons.hearing,
+        color: Colors.white,
+      ),
+    );
+  }
+}
