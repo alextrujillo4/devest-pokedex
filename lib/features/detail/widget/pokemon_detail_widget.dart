2e81ee249e0b99fd5ade610b168f4c2343083ef2
Valentin Trujillo <alexandro4v@gmail.com>
2024-04-27 16:37:12 +0200

    new common widget created

--- lib/features/detail/widget/pokemon_detail_widget.dart
+++ lib/features/detail/widget/pokemon_detail_widget.dart
@@ -1,6 +1,13 @@
-import 'package:flutter/material.dart';
 import 'package:cached_network_image/cached_network_image.dart';
+import 'package:d_chart/single_bar/view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_pokedex/common/constants/app_colors.dart';
 import 'package:flutter_pokedex/common/constants/extensions.dart';
+import 'package:flutter_pokedex/common/widgets/capture_widget.dart';
+import 'package:flutter_pokedex/common/widgets/pokemon_foot_icons.dart';
+import 'package:flutter_pokedex/common/widgets/pokemon_info_card.dart';
+import 'package:flutter_pokedex/common/widgets/pokemon_type_icons.dart';
+import 'package:flutter_pokedex/common/widgets/section_title.dart';
 import 'package:pokemon/pokemon_package.dart';
 
 class PokemonDetailWidget extends StatelessWidget {
@@ -13,75 +20,196 @@ class PokemonDetailWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SingleChildScrollView(
-      padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 8),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            pokemon.name.capitalize(),
-            maxLines: 1,
-            textAlign: TextAlign.start,
-            style: const TextStyle(
-              fontSize: 40.0,
-              fontWeight: FontWeight.bold,
-              overflow: TextOverflow.ellipsis,
-            ),
-          ),
-          const SizedBox(height: 16),
-          Center(
-            child: CachedNetworkImage(
-              width: 180,
-              height: 180,
-              fit:BoxFit.cover,
-              imageUrl: pokemon.picture.frontDefault ?? '',
-              placeholder: (context, url) => const CircularProgressIndicator(),
-              errorWidget: (context, url, error) => const Icon(Icons.error),
-            ),
-          ),
-          const SizedBox(height: 20),
-          Text(
-            'Base Experience: ${pokemon.baseExperience}',
-            style: Theme.of(context).textTheme.subtitle1,
-          ),
-          const SizedBox(height: 10),
-          Text(
-            'Height: ${pokemon.height} decimetres',
-            style: Theme.of(context).textTheme.subtitle1,
-          ),
-          const SizedBox(height: 10),
-          Text(
-            'Weight: ${pokemon.weight} hectograms',
-            style: Theme.of(context).textTheme.subtitle1,
-          ),
-          const SizedBox(height: 10),
-          Text(
-            'Location of Encounters: ${pokemon.locationAreaEncounters}',
-            style: Theme.of(context).textTheme.subtitle1,
-          ),
-          const SizedBox(height: 20),
-          Text(
-            'Types:',
-            style: Theme.of(context).textTheme.headline6,
-          ),
-          Column(
-            children: pokemon.types
-                .map((type) => Text(
-              type.name,
-              style: Theme.of(context).textTheme.bodyText1,
-            ))
-                .toList(),
-          ),
-          if (pokemon.isDefault)
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 10),
-              child: Text(
-                'This is a default Pokémon.',
-                style: Theme.of(context).textTheme.subtitle1?.copyWith(color: Colors.green),
+    final primaryColor =
+        AppColors.getPokemonColorType[pokemon.types.first.name];
+
+    return Stack(
+      children: [
+        Container(color: Colors.black),
+        Container(color: primaryColor?.withOpacity(0.7)),
+        SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(horizontal: 24, vertical: 8),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const SizedBox(height: 24),
+              Center(
+                child: CachedNetworkImage(
+                  width: 240,
+                  height: 240,
+                  fit: BoxFit.cover,
+                  imageUrl: pokemon.picture.frontDefault ?? '',
+                  placeholder: (context, url) =>
+                      const CircularProgressIndicator(),
+                  errorWidget: (context, url, error) => const Icon(Icons.error),
+                ),
               ),
-            ),
-        ],
-      ),
+              Center(
+                child: Text(
+                  "STANDARD FORM",
+                  style: TextStyle(
+                    color: primaryColor,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 4),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                child: SizedBox(
+                  height: 16,
+                  child: DChartSingleBar(
+                    radius: BorderRadius.circular(16),
+                    backgroundColor: Colors.grey,
+                    value: pokemon.baseExperience.toDouble(),
+                    max: 1000,
+                    foregroundColor: AppColors.goldenYellow,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 8),
+              Center(
+                child: SectionTitle(
+                  title: "EXP. ${pokemon.baseExperience}/1000",
+                  color: AppColors.goldenYellow,
+                ),
+              ),
+              const SizedBox(height: 24),
+              PokemonInfoCard(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          pokemon.name.capitalize(),
+                          maxLines: 1,
+                          textAlign: TextAlign.start,
+                          style: const TextStyle(
+                            fontSize: 24.0,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                        ),
+                        Row(
+                          children: pokemon.types
+                              .map((e) => PokemonTypeIcon(typeName: e.name))
+                              .toList(),
+                        ),
+                      ],
+                    ),
+                    Text(
+                      '#${pokemon.id.formattedPokemonId}',
+                      maxLines: 1,
+                      textAlign: TextAlign.start,
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        color: primaryColor,
+                        fontWeight: FontWeight.bold,
+                        overflow: TextOverflow.ellipsis,
+                      ),
+                    ),
+                    const SizedBox(height: 8),
+                    Text(
+                      pokemon.description,
+                      textAlign: TextAlign.start,
+                      style: const TextStyle(
+                        fontSize: 16.0,
+                        color: Colors.white,
+                      ),
+                    ),
+                    const SizedBox(height: 26),
+                    const SectionTitle(title: "FOOTPRINT"),
+                    PokemonFootIcon(id: pokemon.id)
+                  ],
+                ),
+              ),
+              Row(
+                children: [
+                  Expanded(
+                    flex: 1,
+                    child: PokemonInfoCard(
+                      child: Column(
+                        children: [
+                          const SectionTitle(title: "HEIGHT"),
+                          Text(
+                            "${pokemon.height} DM",
+                            style: const TextStyle(
+                              fontSize: 16.0,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    flex: 1,
+                    child: PokemonInfoCard(
+                      child: Column(
+                        children: [
+                          const SectionTitle(title: "WEIGHT"),
+                          Text(
+                            "${pokemon.weight} HG",
+                            style: const TextStyle(
+                              fontSize: 16.0,
+                              color: Colors.white,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              PokemonInfoCard(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    CachedNetworkImage(
+                      width: 80,
+                      height: 80,
+                      fit: BoxFit.cover,
+                      imageUrl: pokemon.picture.backDefault ?? '',
+                      placeholder: (context, url) =>
+                          const CircularProgressIndicator(),
+                      errorWidget: (context, url, error) =>
+                          const Icon(Icons.error),
+                    ),
+                    const Icon(
+                      Icons.change_circle,
+                      color: Colors.white,
+                    ),
+                    CachedNetworkImage(
+                      width: 80,
+                      height: 80,
+                      fit: BoxFit.cover,
+                      imageUrl: pokemon.picture.frontDefault ?? '',
+                      placeholder: (context, url) =>
+                          const CircularProgressIndicator(),
+                      errorWidget: (context, url, error) =>
+                          const Icon(Icons.error),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 4),
+              CaptureWidget(
+                onPressed: () {},
+                label: 'Capture',
+                icon: const Icon(
+                  Icons.catching_pokemon_outlined,
+                  color: Colors.white,
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
     );
   }
 }
