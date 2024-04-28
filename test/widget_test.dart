e47038b61dca760a375ab3642a4cf3e6b3b6724b
Valentin Trujillo <alexandro4v@gmail.com>
2024-04-28 09:51:25 +0200

    pokemon test fixed

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -3,7 +3,4 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:flutter_pokedex/main.dart';
 
 void main() {
-  testWidgets('Test', (WidgetTester tester) async {
-    await tester.pumpWidget(const PokedexApp());
-  });
 }
